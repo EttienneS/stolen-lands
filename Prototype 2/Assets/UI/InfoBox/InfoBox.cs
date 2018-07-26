6645de58fb8a508d3198cd6abec6fee09d4e1afb
Ettienne Scharneck <ettiennes@live.co.za>
2018-07-26 10:52:16 +0200

    Add InfoBox panel Commit before github integration removal and unity update

---
+++ Prototype 2/Assets/UI/InfoBox/InfoBox.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InfoBox : MonoBehaviour {
+
+    public Text TitleText;
+    public Text BodyText;
+
+}
---
+++ Prototype 2/Assets/UI/InfoBox/InfoBoxCloseButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InfoBoxCloseButton : MonoBehaviour
+{
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        var parent = transform.parent;
+        Destroy(parent);
+    }
+}
