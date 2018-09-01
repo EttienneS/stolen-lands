1dc0598c55b0cbe05b87df34dfa9f30f5f51265a
Ettienne Scharneck <ettiennes@live.co.za>
2018-09-01 16:58:31 +0200

    Add Load Screen

---
+++ Assets/LoadScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScreen : MonoBehaviour
+{
+    void Update()
+    {
+        if (!_loading)
+        {
+            _loading = true;
+            StartCoroutine(LoadNewScene());
+            LoadNewScene();
+        }
+        else
+        {
+            var panel = GameObject.Find("LoadPanel").GetComponent<Image>();
+            panel.color = new Color(0f, Random.Range(0.4f, 0.6f), 0f);
+        }
+    }
+
+    private bool _loading;
+
+    public static IEnumerator LoadNewScene()
+    {
+        var async = SceneManager.LoadSceneAsync("Main");
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+    }
+}
