f9164a8832d82381f4bec19dc4cf7bc22b89cf49
Ettienne Scharneck <ettiennes@live.co.za>
2018-09-04 09:33:56 +0200

    Make actors selectable Change camera zoom to use FOV instead of Z layer Fiddle with shadow settings

--- Assets/Camera/CameraController.cs
+++ Assets/Camera/CameraController.cs
@@ -14,10 +14,12 @@ public class CameraController : MonoBehaviour
 
     private float _startTime;
 
+    public Camera Camera;
+
 
     [Range(1, 20)] public int Speed = 5;
 
-    [Range(50, 800)] public int ZoomMax = 500;
+    [Range(50, 100)] public int ZoomMax = 100;
 
     [Range(10, 50)] public int ZoomMin = 10;
 
@@ -36,6 +38,11 @@ public class CameraController : MonoBehaviour
         }
     }
 
+    public void Start()
+    {
+        Camera = GetComponent<Camera>();
+    }
+
     public void MoveToViewCell(HexCell cell)
     {
         _startTime = Time.time;
@@ -44,10 +51,6 @@ public class CameraController : MonoBehaviour
         _journeyLength = Vector3.Distance(_panSource, _panDesitnation);
 
         _panning = true;
-    }
-
-    private void Start()
-    {
     }
 
     private void Update()
@@ -67,60 +70,55 @@ public class CameraController : MonoBehaviour
         else
         {
 #if UNITY_STANDALONE || UNITY_WEBPLAYER
-
             float horizontal = 0;
             float vertical = 0;
             var z = transform.position.z;
             horizontal = Input.GetAxis("Horizontal");
             vertical = Input.GetAxis("Vertical");
-
-            z = -1 * Mathf.Clamp(-transform.position.z - Input.GetAxis("Mouse ScrollWheel") * ZoomStep, ZoomMin,
-                    ZoomMax);
-            transform.position = new Vector3(transform.position.x + horizontal * Speed,
-                transform.position.y + vertical * Speed, z);
+            
+            Camera.fieldOfView =
+ Mathf.Clamp(Camera.fieldOfView - (Input.GetAxis("Mouse ScrollWheel") * ZoomStep), ZoomMin, ZoomMax);
+            transform.position =
+ new Vector3(transform.position.x + horizontal * Speed, transform.position.y + vertical * Speed, z);
 
 #elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
             var speed = 0.25f;
             if (Input.touchCount > 0)
             {
-                Touch touchZero = Input.GetTouch(0);
+                var touchZero = Input.GetTouch(0);
 
                 if (Input.touchCount == 2)
                 {
-                    Touch touchOne = Input.GetTouch(1);
+                    var touchOne = Input.GetTouch(1);
 
-                    Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-                    Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+                    var touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+                    var touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
 
-                    float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-                    float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+                    var prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+                    var touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
 
-                    float deltaMagnitudeDiff = (prevTouchDeltaMag - touchDeltaMag) * speed;
+                    var deltaMagnitudeDiff = (prevTouchDeltaMag - touchDeltaMag) * speed;
 
-                    //Camera.fieldOfView += deltaMagnitudeDiff * speed;
-                    //Camera.fieldOfView = Mathf.Clamp(Camera.fieldOfView, 10f, 150f);
-
-                    transform.Translate(0, 0, deltaMagnitudeDiff);
-
-
-                    // todo: clamp the camera to stop it from moving off screen
-                    //var x = transform.position.x;
-                    //var y = transform.position.y;
-                    //var z = transform.position.z;
-
-                    //transform.position = new Vector3(x, y, z);
+                    Camera.fieldOfView += deltaMagnitudeDiff * speed;
+                    Camera.fieldOfView = Mathf.Clamp(Camera.fieldOfView, ZoomMin, ZoomMax);
                 }
                 else
                 {
                     if (touchZero.phase == TouchPhase.Moved)
                     {
-                        Vector2 touchDeltaPosition = touchZero.deltaPosition;
+                        var touchDeltaPosition = touchZero.deltaPosition;
                         transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
                     }
                 }
             }
 
 #endif //End of mobile platform dependendent compilation section started above with #elif
+
+            // todo: clamp the camera to stop it from moving off screen
+            //var x = transform.position.x;
+            //var y = transform.position.y;
+
+            //transform.position = new Vector3(x, y, z);
         }
     }
 }
